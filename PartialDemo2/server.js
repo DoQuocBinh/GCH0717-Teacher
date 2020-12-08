d2a942efe7b95f2299105762633a0d84161962a8
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-08 09:05:49 +0700

    update parital

--- PartialDemo2/server.js
+++ PartialDemo2/server.js
@@ -9,9 +9,23 @@ app.get('/home',(req,res)=>{
     res.render('home',
     {
         pageTitle: 'Home Page', //displayed in header
-        getCurrentYear: new Date().getFullYear() //displayed in footer
+        getCurrentYear: new Date().getFullYear(), //displayed in footer
+        welcomeMessage: 'Happy New Year'
     })
 })
+app.get('/about',(req,res)=>{
+    res.render('about',
+    {
+        pageTitle: 'About page',
+        getCurrentYear: 2040,
+        technicalName: 'Do Quang Long'
+    })
+})
+
+hbs.registerHelper('screamIt',(msg)=>{
+    return msg.toUpperCase();
+})
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log('Server is running!')

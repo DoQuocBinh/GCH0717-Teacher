940ceeb2b4c9795230733f7dccd507e3b949dc14
DoQuocBinh <binhdq@fpt.edu.vn>
2020-12-22 11:49:39 +0700

    insert

--- MongoDB_NodeJS2/server.js
+++ MongoDB_NodeJS2/server.js
@@ -19,6 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.get('/search',(req,res)=>{
     res.render('search')
 })
+
+app.get('/insert',(req,res)=>{
+    res.render('insert')
+})
 app.post('/doSearch',async (req,res)=>{
     let nameSearch = req.body.txtSearch;
     let client= await MongoClient.connect(url);
@@ -27,6 +31,20 @@ app.post('/doSearch',async (req,res)=>{
     res.render('index',{model:results})
 })
 
+app.post('/doInsert',async (req,res)=>{
+    let nameInput = req.body.txtName;
+    let colorInput = req.body.txtColor;
+    let priceInput = req.body.txtPrice;
+    let newProduct = {
+        productName : nameInput,
+        price: priceInput,
+        color: colorInput
+    }
+    let client= await MongoClient.connect(url);
+    let dbo = client.db("ProductDB2");
+    await dbo.collection("products").insertOne(newProduct);
+    res.redirect('/')
+})
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT)
